9b7d61b32b7b11cf65dce1093b1db1ab2afba9cc
MagdaRajtor <m.rajtor@student.uw.edu.pl>
2022-06-20 14:44:45 +0200

    fixed KPWr

--- word_sketch/read_KPWr.py
+++ word_sketch/read_KPWr.py
@@ -1,39 +1,49 @@
-#packages needed: beautifulsoup4, lxml
-from bs4 import BeautifulSoup
+#packages needed: lxml
+
 import os
-import re
+import xml.etree.ElementTree as ET
 
 def read_xml(path_from, save=False, path_to=None):
-    with open(path_from, 'r', encoding="utf-8") as f:
-        #read and parse data
-        data = f.read()
-        Bs_data = BeautifulSoup(data, "xml")
-        w_orth = Bs_data.find_all('orth') #list of all 'orth' instances
+    #read file and return a list of words
+    tree = ET.parse(path_from)
+    root = tree.getroot()
+    words = []
+    for child in root:
+        for child2 in child:
+            for child3 in child2:
+                element = child3.find('orth')
+                if element is not None:
+                    words.append(str(element.text))
+                else:
+                    words.append('ns/')
 
-        #clean and put into a string
-        res = ""
-        for w in w_orth:
-            result = re.search('<orth>(.*?)</orth>', str(w))
-            word = str(result.group(1))
-            if word in ['.', ',', ';', ':', '/', '?', '!',
-                        'em', 'am', 'śmy', 'ście', 'm']:
-                res += word
+    #put into a string with or without spaces
+    res = ''
+    add_space=True
+    for word in words:
+        if word != 'ns/':
+            if add_space:
+                res += ' ' + word
             else:
-                res += (" " + word)
+                res += word
+            add_space=True
+        if word == 'ns/':
+            add_space=False
 
-        #save data to file (optional)
-        if save:
-            file_path = os.path.dirname(path_from) + '/' #np. files/KPWr/ - to the same directory
-            if path_to is not None:
-                file_path = path_to #path defined by user
-            file_name = os.path.basename(path_from)[:-3] + 'txt' #np. 00101717.txt
-            full_path = file_path+file_name
 
-            out_file = open(full_path, 'w', encoding="utf-8")
-            out_file.write(res.strip())
-            out_file.close()
+    #save data to file (optional)
+    if save:
+        file_path = os.path.dirname(path_from) + '/' #np. files/KPWr/ - to the same directory
+        if path_to is not None:
+            file_path = path_to #path defined by user
+        file_name = os.path.basename(path_from)[:-3] + 'txt' #np. 00101717.txt
+        full_path = file_path+file_name
 
-        return res.strip() #space at the beginning
+        out_file = open(full_path, 'w', encoding="utf-8")
+        out_file.write(res.strip())
+        out_file.close()
+
+    return res.strip()
 
 # 1) save to string
 #Wikipedia
